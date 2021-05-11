56b66d242751a8827b20832d7f59494886a48bd6
aportno <alexander.portno@gmail.com>
2021-05-11 15:47:18 -0400

    pushing

--- ch3-scribbles.hs
+++
@@ -1,6 +0,0 @@
-removeNonUppercase :: [Char] -> [Char] {- synonymous with String -> String -}
-removeNonUppercase str = [c | c <- str, c `elem` ['A'..'Z']]
-
-addThree :: Integer -> Integer -> Integer -> Integer
-addThree x y z = x + y + z
-
