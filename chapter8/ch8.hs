eb5e830e14929227e7410febf5852b04a9bca6e3
aportno <alexander.portno@gmail.com>
2021-05-20 23:26:58 -0400

    progress ch 8

---
+++ chapter8/ch8.hs
@@ -0,0 +1,64 @@
+-- making our own types and typeclasses
+
+module Shapes
+( Point(..)
+, Shape(..)
+, Shape'(..)
+, surface
+, surface'
+, nudge
+, baseCircle
+, baseRect
+) where
+
+-- 8.1 algebraic data types intro
+data Shape = Circle Float Float Float | Rectangle Float Float Float Float
+
+surface :: Shape -> Float
+surface (Circle _ _ r) = pi * r ^ 2
+surface (Rectangle x1 y1 x2 y2) = (abs $ x2 - x1) * (abs $ y2 - y1)
+
+data Point = Point Float Float deriving (Show)
+data Shape' = Circle' Point Float | Rectangle' Point Point deriving (Show)
+
+surface' :: Shape' -> Float
+surface' (Circle' _ r) = pi * r ^ 2
+surface' (Rectangle' (Point x1 y1) (Point x2 y2)) = (abs $ x2 - x1) * (abs $ y2 - y1)
+
+nudge :: Shape' -> Float -> Float -> Shape'
+nudge (Circle' (Point x y) r) a b = Circle' (Point (x+a) (y+b)) r
+nudge (Rectangle' (Point x1 y1) (Point x2 y2)) a b = Rectangle' (Point (x1+a) (y1+a)) (Point (x2+b) (y2+b))
+
+baseCircle :: Float -> Shape'
+baseCircle r = Circle' (Point 0 0) r
+baseRect :: Float -> Float -> Shape'
+baseRect width height = Rectangle' (Point 0 0) (Point width height)
+
+-- 8.2 record syntax
+
+-- the amateur way!
+data Person = Person String String Int Float String String deriving (Show)
+
+firstName :: Person -> String
+firstName (Person firstname _ _ _ _ _) = firstname
+lastName :: Person -> String
+lastName (Person _ lastname _ _ _ _) = lastname
+age:: Person -> Int
+age (Person _ _ age _ _ _) = age
+height :: Person -> Float
+height (Person _ _ _ height _ _) = height
+phoneNumber :: Person -> String
+phoneNumber (Person _ _ _ _ phonenumber _) = phonenumber
+flavor :: Person -> String
+flavor (Person _ _ _ _ _ flavor) = flavor
+
+-- the professional way using record syntax!
+data Person' = Person' { firstName' :: String
+                       , lastName' :: String
+                       , age' :: Int
+                       , height' :: Float
+                       , phoneNumber' :: String
+                       , flavor' :: String
+                       } deriving (Show)
+
+-- 8.3 Type parameters
