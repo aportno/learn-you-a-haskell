83776b6fb536a3d5ee0e8e862499663d8346e994
aportno <alexander.portno@gmail.com>
2021-05-27 14:07:52 -0400

    last chapter

---
+++ chapter10/ch10.hs
@@ -0,0 +1,16 @@
+-- 10.1 functionally solving problems
+
+import Data.List
+solveRPN :: String -> Float
+solveRPN = head . foldl foldingFunction [] . words
+    where
+        foldingFunction (x:y:ys) "*" = (x*y):ys
+        foldingFunction (x:y:ys) "+" = (x+y):ys
+        foldingFunction (x:y:ys) "-" = (y-x):ys
+        foldingFunction (x:y:ys) "/" = (y/x):ys
+        foldingFunction (x:y:ys) "^" = (y**x):ys
+        foldingFunction (x:xs) "ln"  = log x:xs
+        foldingFunction xs "sum"     = [sum xs]
+        foldingFunction xs numberString = read numberString:xs
+
+-- 10.2 Heathrow to London
